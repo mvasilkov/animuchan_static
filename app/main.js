2b018634d14ab915d3e78e5050c29dbedf876ed4
Mark Vasilkov <mvasilkov@gmail.com>
2012-10-27 17:18:24 +0200

    Task list

--- app/main.js
+++ app/main.js
@@ -1,10 +1,17 @@
 (function() {
-    var /** @const */ BACKSPACE = 0x08, /** @const */ BUFFER_SIZE = 40,
+    var /** @const */ BACKSPACE = 0x08, /** @const */ RETURN = 0x0d,
         /** @const */ PRINTABLE = "abcdefghijklmnopqrstuvwxyz0123456789",
-        buffer = "", input = $("#term .input")
+        /** @const */ BUFFER_SIZE = 40, buffer = "",
+        todo = $("#todo"), count = $("#count"), input = $("#term .input")
 
     function isPrintable(ch) {
         return PRINTABLE.indexOf(ch) !== -1
+    }
+
+    function newTask(text) {
+        var task = $("<div class=task>").text(text)
+        todo.append(task)
+        count.css("width", todo.children(".task").length * 11.11 + "%")
     }
 
     $(document.documentElement).keydown(function(event) {
@@ -19,7 +26,10 @@
     $(document.documentElement).keypress(function(event) {
         var ch = String.fromCharCode(event.which).toLowerCase()
 
-        if (ch.length === 1 && isPrintable(ch)) {
+        if (event.which === RETURN) {
+            newTask("fffuuuu~")
+        }
+        else if (ch.length === 1 && isPrintable(ch)) {
             if (buffer.length < BUFFER_SIZE) {
                 buffer += ch
                 input.text(buffer)
