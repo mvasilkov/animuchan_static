a08760db77702e94031b8060f1698600c1b84067
Mark Vasilkov <mvasilkov@gmail.com>
2012-11-18 19:52:09 +0200

    Falling boxes, close #26 (kind of)

--- app/Mob.js
+++ app/Mob.js
@@ -1,8 +1,36 @@
 define(["conf"], function(conf) {
-    var _game = $("#game")
+    var _game = $("#game"),
+        _mobWidth = 55 / conf.GAME_SCALE,
+        _mobHeight = 40 / conf.GAME_SCALE
 
-    function Mob(x, y) {
-        console.log("New mob at", [x, y])
+    function Mob(world, left, top) {
+        var def = new Box2D.b2BodyDef()
+        def.set_type(Box2D.b2_dynamicBody)
+        def.set_position(new Box2D.b2Vec2(left, top))
+        def.set_angle(0)
+
+        var poly = new Box2D.b2PolygonShape()
+        poly.SetAsBox(_mobWidth / 2, _mobHeight / 2)
+
+        var fixdef = new Box2D.b2FixtureDef()
+        fixdef.set_density(1)
+        fixdef.set_friction(0.3)
+        fixdef.set_restitution(0.4)
+        fixdef.set_shape(poly)
+
+        this.body = world.CreateBody(def)
+        this.body.CreateFixture(fixdef)
+
+        this._im = $("<img class=mob src=media/mob.png width=55 height=40>")
+        _game.append(this._im)
+    }
+
+    Mob.prototype.render = function() {
+        var position = this.body.GetPosition()
+        this._im.css({
+            left: (position.get_x() - _mobWidth / 2) * conf.GAME_SCALE,
+            top: (position.get_y() - _mobHeight / 2) * conf.GAME_SCALE
+        })
     }
 
     return Mob
--- app/game.js
+++ app/game.js
@@ -1,22 +1,33 @@
 define(["Mob", "RectProp", "requestAnimationFrame", "conf"],
     function(Mob, RectProp, requestAnimationFrame, conf) {
-        var _game = $("#game"), _mobs = []
+        var _game = $("#game"), _world, _mobs = [],
+            _lastUpdate = new Date().getTime()
 
         function render() {
             requestAnimationFrame(render)
+
+            var now = new Date().getTime(), upd = 0.01 * (now - _lastUpdate)
+            _lastUpdate = now
+
+            _world.Step(upd, 10, 8)
+
+            for (var i = 0; i < _mobs.length; ++i) {
+                _mobs[i].render()
+            }
         }
 
         function init() {
             var real_width = (960 - 325) / conf.GAME_SCALE,
                 real_height = (540 - 87) / conf.GAME_SCALE,
-                gravity = new Box2D.b2Vec2(0, -1),
-                world = new Box2D.b2World(gravity),
+                gravity = new Box2D.b2Vec2(0, 1),
                 props = []
 
-            props.push(new RectProp(world, real_width / 2, 0, real_width, 1))
-            props.push(new RectProp(world, real_width / 2, real_height, real_width, 1))
-            props.push(new RectProp(world, 0, real_height / 2, 1, real_height - 1))
-            props.push(new RectProp(world, real_width, real_height / 2, 1, real_height - 1))
+            _world = new Box2D.b2World(gravity)
+
+            props.push(new RectProp(_world, real_width / 2, 0, real_width, 1))
+            props.push(new RectProp(_world, real_width / 2, real_height, real_width, 1))
+            props.push(new RectProp(_world, 0, real_height / 2, 1, real_height - 1))
+            props.push(new RectProp(_world, real_width, real_height / 2, 1, real_height - 1))
 
             for (var i = 0; i < props.length; ++i) {
                 props[i].render()
@@ -26,7 +37,7 @@ define(["Mob", "RectProp", "requestAnimationFrame", "conf"],
                 var	offset = $(this).offset(),
                     left = (event.pageX - offset.left) / 20,
                     top = (event.pageY - offset.top) / 20,
-                    mob = new Mob(left, top)
+                    mob = new Mob(_world, left, top)
 
                 _mobs.push(mob)
             })
