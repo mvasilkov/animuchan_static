b595a67cdbba828a310b6586a234fc6c3840e592
Mark Vasilkov <mvasilkov@gmail.com>
2012-11-18 18:08:22 +0200

    RectProp#render method

--- app/conf.js
+++ app/conf.js
@@ -2,6 +2,7 @@ define({
     ALLOW_KEYBOARD_INPUT: 1,
     BACKSPACE: 0x08,
     RETURN: 0x0d,
+    GAME_SCALE: 20,
     PRINTABLE: "abcdefghijklmnopqrstuvwxyz0123456789 ",
     BUFFER_SIZE: 40,
     SCORE_SIZE: 9,
--- app/game.js
+++ app/game.js
@@ -1,4 +1,6 @@
-define(["requestAnimationFrame"], function(requestAnimationFrame) {
+define(["requestAnimationFrame", "conf"], function(requestAnimationFrame, conf) {
+    var _game = $("#game")
+
     RectProp = function(world, left, top, width, height) {
         var def = new Box2D.b2BodyDef()
         def.set_position(new Box2D.b2Vec2(left, top))
@@ -14,10 +16,30 @@ define(["requestAnimationFrame"], function(requestAnimationFrame) {
 
         this.body = world.CreateBody(def)
         this.body.CreateFixture(fixdef)
+
+        this._width = width
+        this._height = height
+    }
+
+    RectProp.prototype.render = function() {
+        if (typeof this._rect === "undefined") {
+            this._rect = $("<div class=rect-prop>")
+                /* initialize */
+                .width(this._width * conf.GAME_SCALE)
+                .height(this._height * conf.GAME_SCALE)
+                .appendTo(_game)
+        }
+
+        var position = this.body.GetPosition()
+        this._rect.css({
+            left: (position.get_x() - this._width / 2) * conf.GAME_SCALE,
+            top: (position.get_y() - this._height / 2) * conf.GAME_SCALE
+        })
     }
 
     function init() {
-        var real_width = 960 / 20, real_height = 540 / 20,
+        var real_width = (960 - 325) / conf.GAME_SCALE,
+            real_height = (540 - 87) / conf.GAME_SCALE,
             gravity = new Box2D.b2Vec2(0, -1),
             world = new Box2D.b2World(gravity),
             props = []
@@ -26,6 +48,10 @@ define(["requestAnimationFrame"], function(requestAnimationFrame) {
         props.push(new RectProp(world, real_width / 2, real_height, real_width, 1))
         props.push(new RectProp(world, 0, real_height / 2, 1, real_height - 1))
         props.push(new RectProp(world, real_width, real_height / 2, 1, real_height - 1))
+
+        for (var i = 0; i < props.length; ++i) {
+            props[i].render()
+        }
     }
 
     return {
