0eb9c1fd91296e7a65d1284f97e6a81ce213954c
Mark Vasilkov <mvasilkov@gmail.com>
2012-11-18 21:50:57 +0200

    Update `fab optimize`

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,12 @@
 from fabric.api import local
 
 def optimize():
+    # initialize
     local("ln -s ../lib/almond.js app/almond.js")
+    # build html
+    command = 's|data-main="app/main.js" src="lib/require.js"|src="app.js"|'
+    local("sed '%s' < index.html > upload/index.html" % command)
+    # build js
     local("r.js -o baseUrl=app name=almond include=main out=upload/app.js wrap=true")
+    # clean up
     local("rm app/almond.js")
