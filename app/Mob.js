f93775236eabd1e13c0dca391405975d6004236c
Roman Koblov <penpen938@me.com>
2012-11-19 23:57:34 +0400

    colored mobs

--- app/Mob.js
+++ app/Mob.js
@@ -23,8 +23,8 @@ define(["conf", "utils"], function(conf, utils) {
 
         this.body = world.CreateBody(def)
         this.body.CreateFixture(fixdef)
-
-        this.im = $('<img class="mob" src="media/mob.png" width="55" height="40">')[0]
+        var image = conf.MOB_IMAGE[Math.floor(Math.random()*conf.MOB_IMAGE.length)]
+        this.im = $('<img class="mob" src="media/'+image+'" width="55" height="40">')[0]
 
         this.im.ondragstart = function() { return false }
         this.render()
--- app/conf.js
+++ app/conf.js
@@ -7,5 +7,6 @@ define({
     BUFFER_SIZE: 40,
     SCORE_SIZE: 9,
     TODO_SIZE: 9,
+    MOB_IMAGE: ['mob.png', 'mob2.png', 'mob3.png', 'mob4.png', 'mob5.png', 'mob6.png'],
     COMMANDS: ['add', 'bisect', 'branch', 'checkout', 'clone', 'commit', 'diff', 'fetch', 'grep', 'init', 'log', 'merge', 'mv', 'pull', 'push', 'rebase', 'reset', 'rm', 'show', 'status', 'tag'],
 })
