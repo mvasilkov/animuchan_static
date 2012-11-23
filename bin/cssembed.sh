489e179b538d4d099d0513f3e82b3ad02ae35f5b
Mark Vasilkov <mvasilkov@gmail.com>
2012-11-23 19:38:01 +0200

    Inline CSS assets, close #41

---
+++ bin/cssembed.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar lib/cssembed/patched-cssembed-0.4.6.jar "$@"
--- fabfile.py
+++ fabfile.py
@@ -13,20 +13,25 @@ SED_PROGRAM = ";".join([
     '/^$/d',
 ])
 
+def cleanup():
+    # clean up
+    local("rm -f app/almond.js _css")
+
 def optimize():
     """Rebuild (minify) game files."""
-    # unlink old copy
-    local("rm -f app/almond.js")
+    cleanup()
     # initialize
     local("ln -s ../lib/almond.js app/almond.js")
+    local("mkfifo _css")
     # build html
     local("sed '%s' < index.html > upload/index.html" % SED_PROGRAM)
     # build css
-    local("cat lib/bootstrap/bootstrap.css media/main.css | ycssmin > upload/app.css")
+    local("bin/cssembed.sh media/main.css > _css &")
+    local("cat lib/bootstrap/bootstrap.css _css | ycssmin > upload/app.css")
     # build js
     local("r.js -o baseUrl=app name=almond include=main out=upload/app.js wrap=true")
     # clean up
-    local("rm app/almond.js")
+    cleanup()
 
 def upload():
     """Deploy to server."""
