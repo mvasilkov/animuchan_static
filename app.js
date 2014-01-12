78e0588ddecc3965400d2c46a2e1b5d2cc09c1d7
Mark Vasilkov <mvasilkov@gmail.com>
2014-01-12 22:57:32 +0200

    RC1

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 (function (canvas) {
     define('_canvas', canvas)
+    require.config({
+        urlArgs: 'rc1'
+    })
     require(['js/game', 'js/goo'], function (game, goo) {
         game._goo = goo
         game.state.start('loading')
