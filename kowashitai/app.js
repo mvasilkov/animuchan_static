7a03ac3c8bfde681b52054ae16abfe0584ffd437
Mark Vasilkov <markv@infinidat.com>
2015-07-02 17:25:50 +0300

    Cirno?

--- kowashitai/app.js
+++ kowashitai/app.js
@@ -2,23 +2,35 @@ var CardModel = Backbone.Epoxy.Model.extend({
     defaults: {
         name: '',
         typeclass: '',
+        rank: 0,
         level: 0,
         strength: 0,
         intelligence: 0,
-        agility: 0
+        agility: 0,
+        rarity: 0
     }
 })
 
 var Cirno = new CardModel({
     name: 'Cirno',
     typeclass: 'Fairy',
+    rank: 1,
     level: 9,
     strength: 8,
     intelligence: 4,
-    agility: 9
+    agility: 9,
+    rarity: 5
 })
 
 var view = new Backbone.Epoxy.View({
     el: '#Cirno',
-    model: Cirno
+    model: Cirno,
+    computeds: {
+        rarityInlineCSS: {
+            deps: ['rarity'],
+            get: function (rarity) {
+                return 'width:' + (rarity * 21) + 'px'
+            }
+        }
+    }
 })
